d04836ea7a4aba6d1ae7b8a8ecb37e40c703eb89
abhishekbhartiind <imabhishekbharti12@gmail.com>
2023-02-15 22:40:11 +0530

    show notification

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -6,16 +6,13 @@ this.addEventListener("push", async function (event) {
   console.log("notifications will be displayed here")
   const message = await event.data.json()
   let { title, description, image } = message
-  console.log({ message })
+  console.log(message, "--- here ---")
   await event.waitUntil(
     this.registration.showNotification(title, {
       body: description,
       icon: image,
-      actions: [
-        {
-          title: "say hi",
-        },
-      ],
+      actions: [{ action: "get", title }],
     })
   )
+  console.log("--- here --- show notification")
 })
